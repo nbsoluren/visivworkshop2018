ec9a9b60703149b36253a81a68f42a1d75f565bc
nbsoluren <nbsoluren@gmail.com>
2018-09-19 22:12:52 +0800

    Added Download of Images per Chapter

--- manga.py
+++ manga.py
@@ -12,13 +12,32 @@ def make_folder(directory_name):
     directory = os.getcwd() + "/" + directory_name
     return directory_name
 
-#Retrieving Chapter LINKS
+#Retrieving Chapter Links
 def get_chapter_links(soup):
     chapter_links = []
     tester = soup.find("div", class_="chapter-list").find_all("div",class_="row")
     for i in tester:
         chapter_links.append(i.find("span").a['href'])
     return chapter_links
+
+#Retrieving Image Links
+def get_img_links(soup_chapter):
+    soup_chapter = BeautifulSoup(chapter_page, 'html.parser').find_all("img", class_="img_content")
+    img_links = []
+    for i in soup_chapter:
+        img_links.append(i['src'])
+    return img_links
+
+#Downloading Images Per CHAPTER
+def download_images_per_chapter(img_links, folder):
+    for img in img_links:
+        req = Request(img, headers={'User-Agent': 'Mozilla/5.0'})
+        webpage = urlopen(req).read()
+        filename = img.split('/')[-1]
+        output = open(folder+filename,"wb")
+        output.write(webpage)
+        output.close()
+
 
 url = input("Happy to serve. Enter URL: ")
 url = 'http://mangakakalot.com/manga/the_freudstein_twins'
@@ -35,18 +54,20 @@ soup = BeautifulSoup(webpage, 'html.parser')
 chapter_links = get_chapter_links(soup)
 
 #Iterrating through each chapter
-chapter_img_links = []
+count = 1
 for chapter in chapter_links:
+    #Open up sourcecode of Chapter
     req = Request(chapter, headers={'User-Agent': 'Mozilla/5.0'})
     chapter_page = urlopen(req).read()
 
-    #Getting Img Link for Each chapter
-    soup_chapter = BeautifulSoup(chapter_page, 'html.parser').find_all("img", class_="img_content")
-    img_links = []
-    for i in soup_chapter:
-        img_links.append(i['src'])
-    chapter_img_links.append(soup_chapter)
+    #Make a Folder for Each Chapter
+    folder = make_folder("Chapter" + str(count))
 
-file = open("butt" + ".html","w+")
-file.write(str(chapter_img_links))
-file.close()
+    #Getting Img Link for Each chapter
+    img_links = get_img_links(chapter_page)
+
+    #Download images per chapter
+    download_images_per_chapter(imglinks,folder)
+
+    #increment counter
+    count = count+1
