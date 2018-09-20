7d7e6480fef088ea37f4efe56f6b04c6590ef695
rmdelossantos <russmanilads@gmail.com>
2018-09-20 18:50:44 +0800

    Retrieved URL and parsed into beautifulsoup format

--- add.py
+++ add.py
@@ -12,3 +12,13 @@ def make_folder(directory_name):
        print ('Error: Creating directory. ' +  directory_name)
    directory = os.getcwd() + "/" + directory_name
    return directory_name
+url = 'https://manganelo.com/manga/about_death'
+
+#Retrieving the page source
+#We are pretending to be Mozilla
+req = Request(url, headers={'User-Agent': 'Mozilla/5.0'})
+webpage = urlopen(req).read()
+
+#Passing it into Beautifulsoup format.
+soup = BeautifulSoup(webpage, 'html.parser')
+#try commit
